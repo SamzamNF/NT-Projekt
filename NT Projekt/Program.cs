7875baa3db08c92bcb88a1fa25c3d8276af159c2
Nicolai Friis <nmfr71398@edu.ucl.dk>
2025-05-03 20:33:48 +0200

    Projekt oprettet og pushed

---
+++ NT Projekt/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NT_Projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test af projekt");
+            Console.ReadLine();
+        }
+    }
+}
